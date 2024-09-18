ebe226b06ab1e8c23bc73829a6852587e03f9035
Lara <lara.assuncao@alunoescolassociais.org.br>
2024-09-18 10:06:57 -0300

    definido conexao database

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -5,13 +5,16 @@ class  UserController
 {
  public function register(){
   if($_SERVER['REQUEST_METHOD'] == 'POST'){
+   //coleta os dados evitados pelo formulario e organiza em um array.
     $data = [
         'nome' => $_POST['nome'],
         'email' => $_POST['email'],
         'senha' => password_hash($_POST)['senha'],PASSWORD_DEFAULT),
         'perfil' => $_POST['perfil']
     ];
+    //chama o metodo create do model user para criar o novo usuario.
     User::create($data);
+    header('Location: index.php');
 
  }else{
     // Se a requisição não fot post (por exemplo,GET)
--- models/database.php
+++ models/database.php
@@ -1,5 +1,22 @@
 <?php
 class Database{
     //utiliza padrao singleton, cujo objetivo é garantir que apenas uma unica instancia de classe seja criada durante a execução do programa, e que essa instancia seja utilizada.
+
+privatic static $instance = null;
+//metodo publico que retorna a conexao com BD.
+public static function getConnection(){
+    // Verificar se a instancia de conexao ainda nao foi criada.
+    if(!self::$instance){
+        $host = 'localhost';
+        $db   ='sistema_usuarios';
+        $user  ='root';
+        $password  = '';
+//a conexao usa o driver msql(m)
+        self::$instance = new PDO("mysql:host=$host;dbname=$db,$user,$password");
+        //Define o modo de erro para excecoes,facilitando a depuracao e tratamento de erro.
+        self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    return self::$instance;
+}
 }
 ?>
